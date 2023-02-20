c6ae5398cfe32deaee826e4eaf9d3391b8131fb9
Dominik Janiec <dominikjaniec@gmail.com>
2023-02-20 21:01:47 +0100

    cleanup with dropped 'InitPropsFromFile' and 'no-prompt' module version

--- src/posh-git.psm1
+++ src/posh-git.psm1
@@ -1,11 +1,8 @@
 #######################################################################
-# pwsh -NoProfile -NoExit -WorkingDirectory "$HOME\Repos\posh-git" -Command { function prompt { "custom-prompt> " }; $global:PoshGit_InitProps = @{ DisablePoshGitPrompt = $true; ForcePoshGitPrompt = $true; UseLegacyTabExpansion = $true; UseFunctionCompletion = $true; ShowCompletionErrors = $true; LocalGitVersion = [System.Version]"2.39.1"; LocalGitCygwin = $true; LocalGitExtensions = @{ flow = $true } }; Import-Module -Name "$HOME\Repos\posh-git\src\posh-git.psm1" -ArgumentList @(1,1,1); exit }
+# pwsh -NoProfile -NoExit -WorkingDirectory "$HOME\Repos\posh-git" -Command { $global:PoshGit_InitProps = @{ DisablePoshGitPrompt = $true; UseFunctionCompletion = $true; ShowCompletionErrors = $true; LocalGitVersion = [System.Version]"2.39.2"; LocalGitExtensions = @{ flow = $true } }; Import-Module -Name "$HOME\Repos\posh-git\src\posh-git.psm1"; "--- Got rendered prompt:", $(prompt), $([string]::new("-", 42)) -join "`n" | Write-Host; exit; }
 
 param([bool]$ForcePoshGitPrompt, [bool]$UseLegacyTabExpansion, [bool]$EnableProxyFunctionExpansion)
 
-
-Set-StrictMode -Version 3.0
-$ErrorActionPreference = "Stop"
 
 function __logShowAllEvents_WriteHost {}
 function __logScopePush {}
@@ -13,7 +10,8 @@ function __logScopePop {}
 function __logEvent {}
 
 # # provides implementations of `__log*` methods from above:
-# . "$HOME\Repos\EnvConfigs\_tools\profiler.autogen.ps1" `
+# . "$HOME\Repos\EnvConfigs\_tools\domins-profiler.ps1" `
+#     -__PROFILER_SetStrictModeStop `
 #     # -__PROFILER_SetDebugVerbose `
 #     # -__PROFILER_WriteOn_LogEvent
 
@@ -39,123 +37,23 @@ $Default_InitProps = @{
     }
 }
 
-if ($(Test-Path variable:global:InitPropsFromFile) `
-        -and $global:InitPropsFromFile -eq $true) {
-    __logEvent "calling InitPropsAccess.ps1"
-    . $PSScriptRoot\InitPropsAccess.ps1 `
-        -DefaultProps $Default_InitProps
-}
-else {
-    __logEvent "managing `$global:PoshGit_InitProps"
-
-    function script:Get-InitPropsSkipPrompt () {
-        return $global:PoshGit_InitProps.DisablePoshGitPrompt `
-            -and -not $global:PoshGit_InitProps.ForcePoshGitPrompt
-    }
-
-    function script:Get-InitPropsExtension ([string]$Name) {
-        $exts = $global:PoshGit_InitProps.LocalGitExtensions
-
-        $keys = , $Name
-        $prefix = "git-"
-        $keys += $Name.StartsWith($prefix, [StringComparison]::OrdinalIgnoreCase) `
-            ? $Name.Substring($prefix.Length) `
-            : $prefix + $Name
-
-        $matched = $exts.Keys `
-        | Where-Object { $_ -in $keys }
-        $matched = @($matched)
-
-        if ($matched.Length -eq 0) {
-            return $null
-        }
-        if ($matched.Length -eq 1) {
-            return $exts[$matched[0]]
-        }
-
-        $values = @($matched) `
-        | ForEach-Object { $exts[$_] } `
-        | Select-Object -Unique
-        $values = @($values)
-
-        if ($values.Length -eq 1) {
-            return $values[0]
-        }
-
-        $keys = $keys -join ", "
-        throw "ambiguous configuration with: $keys"
-    }
-
-    __logScopePush "looking for PoshGit-InitProps"
-    if (Test-Path variable:global:PoshGit_InitProps) {
-        foreach ($key in $Default_InitProps.Keys) {
-            if (-not $global:PoshGit_InitProps.ContainsKey($key)) {
-                __logEvent "using Default[$key]"
-                $global:PoshGit_InitProps[$key] = `
-                    $Default_InitProps[$key]
-            }
-            else {
-                $val = $global:PoshGit_InitProps[$key]
-                if ($key -eq "LocalGitExtensions") {
-                    $val = $val | ConvertTo-Json -Compress
-                }
-
-                __logEvent "`$InitProps[$key] = $val"
-            }
-        }
-    }
-    else {
-        $global:PoshGit_InitProps = `
-            $Default_InitProps.Clone()
-    }
-    __logScopePop
-
-    __logScopePush "old-module-param"
-    function Get-ParamOverrideOf ($ParamValue, $ParamName, $GlobalName = $null) {
-        $GlobalName ??= $ParamName
-
-        if ($ParamValue -eq $true) {
-            __logEvent "ArgumentList `$$ParamName' was set"
-            $props = "global:PoshGit_InitProps"
-            $propName = $GlobalName
-
-            Write-Verbose "The 'Import-Module -ArgumentList (...)' could be replaced with:"
-            if ($ParamName -ne $GlobalName) {
-                Write-Verbose "`t> `$$ParamName = `"$GlobalName`""
-                $propName = "`$$ParamName"
-            }
-            Write-Verbose "`t> `$$props = `@{"
-            Write-Verbose "`t>`t `"$propName`" = `$true;"
-            Write-Verbose "`t>`t (...) }"
-            Write-Verbose "`t> Import-Module"
-
-            return $true
-        }
-
-        return $global:PoshGit_InitProps[$GlobalName]
-    }
-
-    __logEvent "setting ``param()`` from `$global:PoshGit_InitProps"
-    $ForcePoshGitPrompt = Get-ParamOverrideOf `
-        $ForcePoshGitPrompt "ForcePoshGitPrompt"
-
-    $UseLegacyTabExpansion = Get-ParamOverrideOf `
-        $UseLegacyTabExpansion "UseLegacyTabExpansion"
-
-    $EnableProxyFunctionExpansion = Get-ParamOverrideOf `
-        $EnableProxyFunctionExpansion "EnableProxyFunctionExpansion" `
-        -GlobalName "UseFunctionCompletion"
-
-    __logScopePop
-}
+__logEvent "calling InitPropsAccess.ps1"
+. $PSScriptRoot\InitPropsAccess.ps1 `
+    -DefaultProps $Default_InitProps
 
 __logScopePop
 
 
-__logScopePush "no-prompt-req"
+__logScopePush "strict-mode-req"
 
 $global:GitMissing = $false
 $global:GitStatus = $null
+
+__logEvent
+if (-not (Test-Path variable:global:LASTEXITCODE)) {
+    __logEvent "ensuring LASTEXITCODE set"
+    $global:LASTEXITCODE = 0
+}
 
 __logScopePop
 
@@ -234,7 +132,9 @@ if ($(Get-InitPropsSkipPrompt)) {
 
     Export-ModuleMember @exportModuleMemberParams
     __logScopePop
+
     __logScopePop
+    __logShowAllEvents_WriteHost
     exit
 }
 
